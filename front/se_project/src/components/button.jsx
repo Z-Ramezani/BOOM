66cb2fffbde910b9416c022f94f780ddf15db1bc
Madanian <nouriehmadanian@gmail.com>
2022-04-21 13:23:04 +0430

    button

--- front/se_project/src/components/button.jsx
+++ front/se_project/src/components/button.jsx
@@ -1,21 +1,17 @@
 import {Component} from 'react';
 
 class Button extends Component {
+
     state = {  } 
     render() { 
         return (
             <>
                 <div className="d-grid gap-2 d-md-flex justify-content-md-end">
-                    <button className="btn btn-primary" type="button">{this.props.text}</button>
-
+                    <button /*onClick={this.props.reference} */ className="btn btn-primary" type="button" style={{width: this.props.width, height:this.props.height, backgroundColor:'#BB1542', borderColor:'#BB1542', fontSize:'23px'}}>{this.props.text}</button>
                 </div>
             </>
         );
     }
-}
-
-assign (){
-     this.props.text;
 }
 
 export default Button;
--- front/se_project/src/components/login.jsx
+++ front/se_project/src/components/login.jsx
@@ -1,13 +1,10 @@
-import {Component} from 'react';
+import React, {Component} from 'react';
+import Button from './button';
 
 class Login extends Component {
     state = {  } 
     render() { 
-        return (
-            <>
-                
-            </>
-        );
+        return (<Button width= '337px' height= '48px' text= 'ورود' /*reference=''*//>); //sholde use axios library for connect backend in next edit
     }
 }
  
