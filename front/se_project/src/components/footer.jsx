5aa08faec5f1ac6246f485f70a30241321103899
Flower <maryam.shafizadegan.8098@gmail.com>
2022-04-24 17:18:50 +0430

    footer

--- front/se_project/src/components/footer.jsx
+++ front/se_project/src/components/footer.jsx
@@ -1,124 +1,67 @@
 import { Component } from "react";
 import { MDBFooter } from 'mdb-react-ui-kit';
+import logo from '../assets/images/logo.png';
 
 class Footer extends Component {
     render() {
         return(
             <MDBFooter bgColor='light' className='text-center text-lg-start text-muted'>
 
-            <section className='d-flex justify-content-center justify-content-lg-between p-4 border-bottom'>
-              <div className='me-5 d-none d-lg-block'>
-                <span>Get connected with us on social networks:</span>
-              </div>
-      
-              <div>
-                <a href='' className='me-4 text-reset'>
-                  <i className='fab fa-facebook-f'></i>
-                </a>
-                <a href='' className='me-4 text-reset'>
-                  <i className='fab fa-twitter'></i>
-                </a>
-                <a href='' className='me-4 text-reset'>
-                  <i className='fab fa-google'></i>
-                </a>
-                <a href='' className='me-4 text-reset'>
-                  <i className='fab fa-instagram'></i>
-                </a>
-                <a href='' className='me-4 text-reset'>
-                  <i className='fab fa-linkedin'></i>
-                </a>
-                <a href='' className='me-4 text-reset'>
-                  <i className='fab fa-github'></i>
-                </a>
-              </div>
-            </section>
-      
-            <section className=''>
-              <div className='container text-center text-md-start mt-5'>
-                <div className='row mt-3'>
-                  <div className='col-md-3 col-lg-4 col-xl-3 mx-auto mb-4'>
-                    <h6 className='text-uppercase fw-bold mb-4'>
-                      <i className='fas fa-gem me-3'></i>Company name
-                    </h6>
-                    <p>
-                      Here you can use rows and columns to organize your footer content. Lorem ipsum dolor sit amet,
-                      consectetur adipisicing elit.
-                    </p>
+            <section className='d-flex justify-content-center justify-content-lg-between p-4 border-bottom'> 
+              
+                <div className='container text-center text-md-start mt-2'>
+                  <div className='col-md-2 col-md-offset-3'>
+                    <a href='' className='me-4 text-reset'>
+                      <i class="bi bi-envelope"></i>
+                    </a>
+                    <a href='' className='me-4 text-reset'>
+                      <i class="bi bi-linkedin"></i>
+                    </a>
+                    <a href='' className='me-4 text-reset'>
+                      <i class="bi bi-instagram"></i>
+                    </a>
+                    <a href='' className='me-4 text-reset'>
+                      <i class="bi bi-telegram"></i>
+                    </a>
                   </div>
-      
-                  <div className='col-md-2 col-lg-2 col-xl-2 mx-auto mb-4'>
-                    <h6 className='text-uppercase fw-bold mb-4'>Products</h6>
-                    <p>
-                      <a href='#!' className='text-reset'>
-                        Angular
-                      </a>
-                    </p>
-                    <p>
-                      <a href='#!' className='text-reset'>
-                        React
-                      </a>
-                    </p>
-                    <p>
-                      <a href='#!' className='text-reset'>
-                        Vue
-                      </a>
-                    </p>
-                    <p>
-                      <a href='#!' className='text-reset'>
-                        Laravel
-                      </a>
-                    </p>
-                  </div>
-      
-                  <div className='col-md-3 col-lg-2 col-xl-2 mx-auto mb-4'>
-                    <h6 className='text-uppercase fw-bold mb-4'>Useful links</h6>
-                    <p>
-                      <a href='#!' className='text-reset'>
-                        Pricing
-                      </a>
-                    </p>
-                    <p>
-                      <a href='#!' className='text-reset'>
-                        Settings
-                      </a>
-                    </p>
-                    <p>
-                      <a href='#!' className='text-reset'>
-                        Orders
-                      </a>
-                    </p>
-                    <p>
-                      <a href='#!' className='text-reset'>
-                        Help
-                      </a>
-                    </p>
-                  </div>
-      
-                  <div className='col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4'>
-                    <h6 className='text-uppercase fw-bold mb-4'>Contact</h6>
-                    <p>
-                      <i className='fas fa-home me-3'></i> New York, NY 10012, US
-                    </p>
-                    <p>
-                      <i className='fas fa-envelope me-3'></i>
-                      info@example.com
-                    </p>
-                    <p>
-                      <i className='fas fa-phone me-3'></i> + 01 234 567 88
-                    </p>
-                    <p>
-                      <i className='fas fa-print me-3'></i> + 01 234 567 89
-                    </p>
+
+
+                  <div className='row mt-1'>
+                    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                      
+                      <ul className="navbar-nav">
+                        <li className="nav-item">
+                          <a className="nav-link" href="#">تماس با ما</a>
+                        </li>
+                        <li className="nav-item">
+                          <a className="nav-link" href="#">سوالات متداول</a>
+                        </li>
+                        <li className="nav-item">
+                          <a className="nav-link" href="#">قوانین</a>
+                        </li>
+                        <li className="nav-item">
+                          <a className="nav-link" href="#">پیشنهادات و انتقادات</a>
+                        </li>
+                      </ul>
+                    </nav>
                   </div>
                 </div>
-              </div>
-            </section>
-      
-            <div className='text-center p-4' style={{ backgroundColor: 'rgba(0, 0, 0, 0.05)' }}>
-              © 2021 Copyright:
-              <a className='text-reset fw-bold' href='https://mdbootstrap.com/'>
-                MDBootstrap.com
+
+              <a className="navbar-brand" href="#">
+                <img
+                  src={logo}
+                  height='50'
+                  alt='React Bootstrap logo'
+                  loading='lazy'
+                />
               </a>
+              
+            </section>
+
+            
+      
+            <div className='text-center p-2' style={{ backgroundColor: 'rgba(0, 0, 0, 0.05)' }}>
+              © تمامی حقوق محفوظ است
             </div>
           </MDBFooter>
         
--- front/se_project/src/components/header-slide.jsx
+++ front/se_project/src/components/header-slide.jsx
@@ -35,3 +35,5 @@ class headerSlide extends Component {
         )
     }
 }
+
+export default headerSlide;
