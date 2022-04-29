1ca2f62efb23f99daaf06d882ffbd664bd2c7376
Madanian <nouriehmadanian@gmail.com>
2022-04-29 17:20:53 +0430

    edit front end branch

--- front/se_project/src/Pages/mainPage.jsx
+++ front/se_project/src/Pages/mainPage.jsx
@@ -4,7 +4,6 @@ import Navbar from '../components/navBar/navBar';
 import Breadcrumbb from '../components/breadcrumb';
 import Footer from '../components/footer';
 import ArtworkGroup from '../components/artworkGroup';
-import HeaderSlide from '../components/header-slide';
 class MainPage extends Component {
     state = { 
         // ads: []
@@ -19,7 +18,6 @@ class MainPage extends Component {
         return (
             <div>
                 <Navbar/>
-                <HeaderSlide/>
                 <Breadcrumbb is1OrNot='true' first='صفحه اصلی'/>
                 <div className='d-flex flex-column min-vh-100'>
                     <div className='container w-75 p-3'>
