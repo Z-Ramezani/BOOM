69711f8ab798a9da40d37980d114abeaf8bd0f0a
Flower <maryam.shafizadegan.8098@gmail.com>
2022-05-07 21:11:20 +0430

    add finction

--- front/se_project/src/Pages/login.jsx
+++ front/se_project/src/Pages/login.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import {Component} from 'react';
 import axios from 'axios';
 import {useHistory} from 'react-router-dom';
@@ -8,26 +8,74 @@ import Navbar from '../components/navBar/navBar';
 import Footer from '../components/footer';
 import color from '../assets/images/color.png'
 import Input_text from '../components/input_form';
-class Login extends Component {
-  render() { 
-    const {userName, setUserName}=this.setState;
-    const {password, setPassword}=this.setState;
+// class Login extends Component {
+//   render() { 
+//     const {userName, setUserName}=this.setState;
+//     const {password, setPassword}=this.setState;
 
-    // let sendData = () => {
-    //     axios.post('http://hidden.pythonanywhere.com/users/login', {userName, password})
-    //         .then(res => {
-    //             App.user = res.data.data;
-    //             console.log(App.user)
-    //             history.push('/')
-    //         })
-    //         .catch(err => console.log(err.data))
-    // }
-    // let history = useHistory();
+//     // let sendData = () => {
+//     //     axios.post('http://hidden.pythonanywhere.com/users/login', {userName, password})
+//     //         .then(res => {
+//     //             App.user = res.data.data;
+//     //             console.log(App.user)
+//     //             history.push('/')
+//     //         })
+//     //         .catch(err => console.log(err.data))
+//     // }
+//     // let history = useHistory();
+
+//     function validateFormFields() {
+//         return userName.length > 0 && password.length > 0;
+//     }
+//     return (
+//         <div className='bg-image' style={{backgroundImage:color}}>
+//             <Navbar/>
+//             <Breadcrumbb is2OrNot='true' first='صفحه اصلی' second='ورود'/>
+//             <div className='container flex-column min-vh-100' >
+//                 <div className="row d-flex justify-content-center " >  
+//                     <form onSubmit={this.handleSubmit}>
+//                         <div className="col-md-4">
+//                             <form  className="form-control">
+//                                 <div className="mb-3">
+//                                     <label for="InputEmail1" className="form-label"  style={{fontSize:'1.1vw'}}>نام کاربری</label>
+//                                     <Input_text type="text" value={userName} onChange={(e)=>setUserName(e.target.value)}/>
+//                                 </div>
+//                                 <div className="mb-3">
+//                                     <label for="InputPassword1" className="form-label"  style={{fontSize:'1.1vw'}}>رمز عبور</label>
+//                                     <Input_text type="text" value={password} onChange={(e)=>setPassword(e.target.value)}/>
+//                                     <a className='nav-link' href='#' style={{fontSize:'0.7vw',color:'#222222',marginTop:'0%'}} >بازیابی رمز عبور</a>
+
+//                                 </div>
+//                                 <div className="mb-3 form-check pull-right">
+//                                     <input type="checkbox"  id="Check1" style={{marginRight:'2%'}} />
+//                                     <label className="form-check-label me-1" data-ng-model="rememberMe"  style={{fontSize:'0.9vw'}}>مرا به یاد بسپار</label>
+//                                 </div>
+//                                 <div className="mb-3">
+//                                     <Button  width= '100%' height= '5%' text= 'ورود' fontSize='1.5vw' disabled={!validateFormFields()} /*reference=''*//*sholde use axios library for connect backend in next edit*//>
+//                                 </div>
+//                             </form>
+//                         </div>
+//                     </form> 
+//                 </div>
+//             </div>
+//             <Footer/>
+//         </div>                    
+//     );
+//   }
+// }
+ 
+// export default Login;
+
+const Login = () => {
+
+    const {userName, setUserName}=useState("");
+    const {password, setPassword}=useState("");
 
     function validateFormFields() {
         return userName.length > 0 && password.length > 0;
     }
-    return (
+
+    return ( 
         <div className='bg-image' style={{backgroundImage:color}}>
             <Navbar/>
             <Breadcrumbb is2OrNot='true' first='صفحه اصلی' second='ورود'/>
@@ -59,9 +107,8 @@ class Login extends Component {
                 </div>
             </div>
             <Footer/>
-        </div>                    
-    );
-  }
+        </div>
+     );
 }
  
 export default Login;
--- front/se_project/src/Pages/viewArtistProfile.jsx
+++ front/se_project/src/Pages/viewArtistProfile.jsx
@@ -10,22 +10,40 @@ import ArtworkAdAdditionalInfo from '../components/artworkAdAdditionalInfo'
 import UserInformation from '../components/userInformation';
 import Resume from '../components/resume';
 
-class ViewArtistProfile extends Component {
-  render() { 
-    return (
-        <div className='d-flex flex-column min-vh-100'>
-            <Navbar/>
-            <Breadcrumbb is3OrNot='true' first='صفحه اصلی' second='آگهی اثر' third='نمایش حساب کاربری هنرمند'/>
-            <HorizontalLine value='اطلاعات فردی'/>
-            <Resume/>
-            <HorizontalLine value='نمونه آثار'/>
-            <div className='container w-75 p-3 shadow p-3 bg-white rounded mb-5'>
-                <ArtworkAdAdditionalInfo/>
-            </div> 
-            <Footer/>
-        </div>                    
-    );
-  }
+// class ViewArtistProfile extends Component {
+//   render() { 
+//     return (
+//         <div className='d-flex flex-column min-vh-100'>
+//             <Navbar/>
+//             <Breadcrumbb is3OrNot='true' first='صفحه اصلی' second='آگهی اثر' third='نمایش حساب کاربری هنرمند'/>
+//             <HorizontalLine value='اطلاعات فردی'/>
+//             <Resume/>
+//             <HorizontalLine value='نمونه آثار'/>
+//             <div className='container w-75 p-3 shadow p-3 bg-white rounded mb-5'>
+//                 <ArtworkAdAdditionalInfo/>
+//             </div> 
+//             <Footer/>
+//         </div>                    
+//     );
+//   }
+// }
+ 
+// export default ViewArtistProfile;
+
+const ViewArtistProfile = () => {
+  return ( 
+    <div className='d-flex flex-column min-vh-100'>
+          <Navbar/>
+          <Breadcrumbb is3OrNot='true' first='صفحه اصلی' second='آگهی اثر' third='نمایش حساب کاربری هنرمند'/>
+          <HorizontalLine value='اطلاعات فردی'/>
+          <Resume/>
+          <HorizontalLine value='نمونه آثار'/>
+          <div className='container w-75 p-3 shadow p-3 bg-white rounded mb-5'>
+              <ArtworkAdAdditionalInfo/>
+          </div> 
+          <Footer/>
+      </div>   
+   );
 }
  
 export default ViewArtistProfile;
