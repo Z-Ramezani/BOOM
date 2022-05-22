da09c941f44f61353d6d1f405622418c5ed256e9
Zahra Ramezani <miss.ramazani@yahoo.com>
2022-05-22 14:37:43 +0430

    editting

--- front/se_project/src/components/expertResume.jsx
+++ front/se_project/src/components/expertResume.jsx
@@ -12,7 +12,7 @@ class ExpertResume extends Component {
                     <div className='container w-75 p-3 p-3 rounded mb-5' >
                         <div className='row justify-content-center' style={{backgroundColor:'#F9FFFF'}}>
                             <div className='row mb-2'>     
-                                <div class='col row '>
+                                <div className='col row '>
                                     <label className='col-4 col-form-label' style={{fontSize:'1.1vw'}}>زمینه فعالیت</label>
                                     <div className='col-4'>
                                         <Input_text type="text" value={activityfield} onChange={(e)=>setactivityfield(e.target.value)}/>
