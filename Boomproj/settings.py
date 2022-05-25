4099a337d9292373377eba54df16daad2d3ef8f1
hidden9 <ali666st@gmail.com>
2022-05-25 14:12:17 +0430

    edit setting at 25/5/22

--- Boomproj/settings.py
+++ Boomproj/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -188,3 +189,4 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 #DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
+AUTH_USER_MODEL = 'Boom.User'
