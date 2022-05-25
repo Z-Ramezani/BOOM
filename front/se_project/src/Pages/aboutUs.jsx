5cb38bb3ea9c10302119fdcc219011f63d62de66
Zahra Ramezani <miss.ramazani@yahoo.com>
2022-05-25 13:15:41 +0430

    compelet membership section

--- front/se_project/src/Pages/aboutUs.jsx
+++ front/se_project/src/Pages/aboutUs.jsx
@@ -5,10 +5,8 @@ import Navbar from '../components/navBar/navBar';
 import Breadcrumbb from '../components/breadcrumb';
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
-import x from "../assets/ig.jpg";
-import a from "../assets/11.jpg";
-import b from "../assets/B.png";
-import c from "../assets/11.jpg";
+
+import AboutUsMember from '../components/aboutUsMember';
 const AboutUs = (props) => {
     return ( 
         <div>
@@ -20,60 +18,21 @@ const AboutUs = (props) => {
                     <h6><strong className='me-2' style={{fontSize:'1.1vw',color:'#CC3737'}}>وقتی به بوم می‌پیوندید، به بازار هنر می‌پیوندید – دنیایی برای هنرمندان مستقل.</strong></h6>
                     <br/>
                     <h6 className='me-2' style={{fontSize:'1.1vw'}}>ما، گروهی از دانشجویان دانشگاه اصفهان دور هم جمع شده‌ایم تا واسطه شویم میان شما علاقمندان و هنرمندان.</h6>
-                    <div className='container w-25 p-3 text-center'>
-                    <div id="carouselExampleIndicators" className='carousel slide' data-bs-ride="carousel">
-                        
-                        <div className='carousel-inner'>
-                            <div className='carousel-item active'>
-                            <div class="row d-flex justify-content-center">
-                                <div class="col-md-8">
-                                    <img src={b} className='rounded-circle' media-simple="true"/>
-                                    <div>
-                                        Nourieh
-                                    </div>
-                                    <div>
-                                        DEVELOPER
-                                    </div>
-                                </div>
-                                </div>
+                    <AboutUsMember/>
+                    
+                    <p className='mt-5' style={{fontSize:'1vw'}}>راه‌های ارتباطی:</p>
+                    <div className='d-flex text-center'>
+                    <div className='mx-5' ><i class="bi bi-telephone-fill"></i>
+                                <div className='me-1' style={{fontSize:'1.3vw'}}>2030 100 0913</div>
                             </div>
-                            <div className='carousel-item'>
-                                <div class="col-md-8" style={{margin:'auto'}}>
-                                    <img className="img-circle" src={x}  media-simple="true"style={{ maxWidth:'100%',maxHeight:'100%', borderRadius:'10vw'}}/>
-                                    <div class="user_name mbr-bold pb-2 mbr-fonts-style display-7">
-                                        dcsdcsdc
-                                    </div>
-                                    <div class="user_desk mbr-light mbr-fonts-style display-7">
-                                        DEVELOPER
-                                    </div>
-                                </div>
+                            <div ><i class="bi bi-envelope-fill"></i>
+                                <div className='me-1' style={{fontSize:'1.3vw'}}>Boom@yahoo.com</div>
                             </div>
-                            <div className='carousel-item'>
-                                <div class="col-md-8" style={{margin:'auto'}}>
-                                    <img className="img-circle" src={x}  media-simple="true"style={{ maxWidth:'100%',maxHeight:'100%', borderRadius:'10vw'}}/>
-                                    <div class="user_name mbr-bold pb-2 mbr-fonts-style display-7">
-                                        scljkdscksd
-                                    </div>
-                                    <div class="user_desk mbr-light mbr-fonts-style display-7">
-                                        DEVELOPER
-                                    </div>
-                                </div>
                             </div>
-                        </div>
-                        <button className='carousel-control-prev' type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-                            <span className='carousel-control-prev-icon' aria-hidden="true"></span>
-                            <span className='visually-hidden'>Previous</span>
-                        </button>
-                        <button className='carousel-control-next' type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-                            <span className='carousel-control-next-icon' aria-hidden="true"></span>
-                            <span className='visually-hidden'>Next</span>
-                        </button>
-                    </div>
                     </div>
                     <Footer/>
                 </div>
             </div>
-        </div>
     );
 }
 export default AboutUs;
